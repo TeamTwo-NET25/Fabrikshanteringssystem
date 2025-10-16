81907b75fe1caba7e3b04470898e621e42c48f3d
Anders Hedström <toastwaffle@gmail.com>
2025-10-16 13:14:45 +0200

    La till skit

--- Fabrikshanteringssystem/Fabrikshanteringssystem/Program.cs
+++ Fabrikshanteringssystem/Fabrikshanteringssystem/Program.cs
@@ -1,10 +1,40 @@
-﻿namespace Fabrikshanteringssystem
+﻿using System;
+using System.Collections.Generic;
+class Program
 {
-    internal class Program
+    static List<string> inventory = new List<string>();
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        Console.WriteLine("Välkommen till Fabrikshanteringssystemet!");
+        while (true)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("\nVälj en åtgärd:");
+            Console.WriteLine("1. Lägg till produkt");
+            Console.WriteLine("2. Visa inventarie");
+            Console.WriteLine("3. Avsluta");
+            string val = Console.ReadLine();
+            switch (val)
+            {
+                case "1":
+                    LäggTillProdukt();
+                    break;
+                case "2":
+                    VisaInventarie();
+                    break;
+                case "3":
+                    return;
+                default:
+                    Console.WriteLine("Ogiltigt val. Försök igen.");
+                    break;
+            }
         }
+    }
+    static void LäggTillProdukt()
+    {
+        // TODO: Implementera metod för att lägga till produkt
+    }
+    static void VisaInventarie()
+    {
+        // TODO: Implementera metod för att visa inventarie
     }
 }
